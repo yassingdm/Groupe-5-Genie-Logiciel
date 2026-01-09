e07f399c2bafb056a9cefa4a4582177ec0a26127
Ahahlban <alban.brg12@gmail.com>
2026-01-09 17:04:00 +0100

    Commit initial

---
+++ grp5/src/main/java/eidd/grp5/App.java
@@ -0,0 +1,13 @@
+package eidd.grp5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
