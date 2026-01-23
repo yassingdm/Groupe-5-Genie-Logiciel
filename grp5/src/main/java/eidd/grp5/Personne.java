e9e97436e8844cb0b2ff9e4cc35aa78eed66f8f3
bastien-adiveze <bastien.adiveze.soulage@gmail.com>
2026-01-23 17:05:21 +0100

    Ajout de champs nom et email et constructeur a Personne.java

--- grp5/src/main/java/eidd/grp5/Personne.java
+++ grp5/src/main/java/eidd/grp5/Personne.java
@@ -1,3 +1,10 @@
 package eidd.grp5;
 public class Personne {
+    private String nom;
+    private String mail;
+
+    public Personne(String name,String email){
+        this.nom=name;
+        this.mail=email;
+    }
 }
