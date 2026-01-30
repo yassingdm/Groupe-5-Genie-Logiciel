71c8cd47a49c55b774e7dbfb0608f143f0038d76
yassingdm <yassgdm@gmail.com>
2026-01-30 18:00:45 +0100

    Adding english file names

---
+++ grp5/src/main/java/eidd/grp5/modele/Room.java
@@ -0,0 +1,16 @@
+package eidd.grp5.modele;   
+public class Room {
+    private int id;
+    private int capacity;
+    private String name;
+    private boolean available;
+    private String description;
+
+
+    public Room(int id,String name,int capacity,String description){
+        this.id=id;
+        this.name=name;
+        this.capacity=capacity;
+        this.description=description;
+    }
+}
