2d3963949b7fb9926a7f12eae8ba17fa7f61d51c
Ahahlban <alban.brg12@gmail.com>
2026-02-06 17:08:06 +0100

    Add attributes to Reservation.java class

--- grp5/src/main/java/eidd/grp5/model/Reservation.java
+++ grp5/src/main/java/eidd/grp5/model/Reservation.java
@@ -1,4 +1,22 @@
 package eidd.grp5.model;
+
+import java.time.LocalDateTime;
+
 public class Reservation {
+private Long id;                 
+    private String reference;         
     
-}
+    
+    private User client;              
+    private Salle salle;              
+    
+    
+    private LocalDateTime dateDebut;  
+    private LocalDateTime dateFin;    
+    private LocalDateTime dateCreation; 
+    
+    
+    private int nombreParticipants;    
+    private String motif;             
+}    
+
